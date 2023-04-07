9568dbbde8b5fd2afc65341799e1e6ddb22639b3
Luiz Eduardo Kowalski <luizeduardokowalski@gmail.com>
2023-04-08 00:33:57 +0200

    Update mappings

--- files/mappings.lua
+++ files/mappings.lua
@@ -1,11 +1,13 @@
 local M = {}
 
 -- Your custom mappings
-M.abc = {
+M.custom = {
     n = {
         ["<C-o>"] = {":Telescope find_files <CR>", "Telescope Files"},
         ["<C-z>"] = { "<cmd> undo <CR>", "Undo"},
-        ["q"] = {"<cmd> <leader> x <CR>", "Quit file"}
+        ["q"] = {"<cmd> <leader> x <CR>", "Quit file"},
+        ["<leader>gs"] = { ":Telescope git_status <CR>", "git status"},
+        ["<C-f>"] = { ":Telescope live_grep <CR>", "live grep"}
     },
 
     l = {
