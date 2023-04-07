057807938574503bb6e214511199b333ccfd07e5
Luiz Eduardo Kowalski <luizeduardokowalski@gmail.com>
2023-04-07 21:08:25 +0200

    Added ruby-lsp

--- files/plugins.lua
+++ files/plugins.lua
@@ -1,6 +1,15 @@
 local plugins = {
   { "github/copilot.vim", lazy = false },
+  {
+    "neovim/nvim-lspconfig",
+     config = function()
+        require "plugins.configs.lspconfig"
+        require "custom.configs.lspconfig"
+     end,
+  },
 }
+
+require'lspconfig'.ruby_ls.setup{}
 
 return plugins
 
