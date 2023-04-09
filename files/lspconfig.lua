2371b55b55d2f9b84be8e240d3a7931ce8bb3da8
Luiz Eduardo Kowalski <luizeduardokowalski@gmail.com>
2023-04-09 15:40:27 +0200

    File adjustments

--- files/lspconfig.lua
+++ files/lspconfig.lua
@@ -2,11 +2,11 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "clangd"}
+local servers = {"html", "cssls", "clangd"}
 
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-  }
+    lspconfig[lsp].setup {
+        on_attach = on_attach,
+        capabilities = capabilities
+    }
 end
--- files/plugins.lua
+++ files/plugins.lua
@@ -1,24 +1,19 @@
-local plugins = {
-  { "github/copilot.vim", lazy = false },
-  {
+local plugins = {{
+    "github/copilot.vim",
+    lazy = false
+}, {
     "neovim/nvim-lspconfig",
-     config = function()
+    config = function()
         require "plugins.configs.lspconfig"
         require "custom.configs.lspconfig"
-     end,
-  },
-  {
+    end
+}, {
     "lewis6991/gitsigns.nvim",
-    opts = { current_line_blame = true },
-  },
-}
+    opts = {
+        current_line_blame = true
+    }
+}}
 
-require('lspconfig').ruby_ls.setup{}
-
-local function open_nvim_tree()
-  require("nvim-tree.api").tree.open()
-end
-
-vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+require('lspconfig').ruby_ls.setup {}
 
 return plugins
