46d675bd9562f2fbcef68fe62eed420083f70a47
Luiz Eduardo Kowalski <luizeduardokowalski@gmail.com>
2023-04-09 15:25:43 +0200

    open nvim-tree on startup

--- files/plugins.lua
+++ files/plugins.lua
@@ -15,5 +15,10 @@ local plugins = {
 
 require('lspconfig').ruby_ls.setup{}
 
-return plugins
+local function open_nvim_tree()
+  require("nvim-tree.api").tree.open()
+end
 
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+
+return plugins
