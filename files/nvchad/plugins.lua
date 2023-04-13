6059cb0d3272ba37fee13f07feea25e6a3088969
Luiz Eduardo Kowalski <luizeduardokowalski@gmail.com>
2023-04-13 17:59:43 +0200

    Faster gitsigns

--- files/nvchad/plugins.lua
+++ files/nvchad/plugins.lua
@@ -24,7 +24,7 @@ local plugins = {
       current_line_blame_opts = {
         virt_text = true,
         virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
-        delay = 250,
+        delay = 150,
         ignore_whitespace = false,
       },
       current_line_blame_formatter = "<author>, <author_time:%d-%b-%Y> - <summary>",
