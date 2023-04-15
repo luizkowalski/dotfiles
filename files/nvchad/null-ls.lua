f992322d44333b853fd941c0817cd086dde49cd3
Luiz Eduardo Kowalski <luizeduardokowalski@gmail.com>
2023-04-15 23:44:51 +0200

    General dotfiles changes

--- files/nvchad/null-ls.lua
+++ files/nvchad/null-ls.lua
@@ -8,13 +8,14 @@ local sources = {
   formatting.stylua,
   formatting.htmlbeautifier,
   formatting.prettier,
+  formatting.shellharden,
   formatting.jq,
-  hover.printenv,
-  hover.dictionary,
   diagnostics.eslint,
   diagnostics.yamllint,
   diagnostics.hadolint,
-  -- diagnostics.shellcheck,
+  diagnostics.shellcheck,
+  hover.dictionary,
+  hover.printenv,
   -- diagnostics.rubocop.with ( { command = "bundle", args = { "exec", "rubocop", "-f", "json", "--force-exclusion", "--stdin", "$FILENAME" } } ),
 }
 
