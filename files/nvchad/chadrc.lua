d9e8f6dd89b40a35534fc7fcc539f872d97b0d0d
Luiz Eduardo Kowalski <luizeduardokowalski@gmail.com>
2023-06-01 10:24:36 +0200

    Updated dockerfile filetype and railsrc

--- files/nvchad/chadrc.lua
+++ files/nvchad/chadrc.lua
@@ -1,13 +1,33 @@
 local M = {}
 
 M.ui = {
-  theme = "aquarium",
+  theme = "github_dark",
 }
 
 M.plugins = "custom.plugins"
 M.mappings = require("custom.mappings")
 
 vim.g.copilot_assume_mapped = true
+
+-- Set Dockerfile.web file type to dockerfile
+-- vim.api.nvim_create_autocmd("FileType", {
+--   pattern = "Dockerfile.*",
+--   callback = function()
+--     vim.b.filetype = "dockerfile"
+--   end
+-- })
+
+vim.filetype.add({
+  pattern = {
+    ["Dockerfile.*"] = "dockerfile",
+  },
+})
+-- vim.api.nvim_create_autocmd("FileType", {
+--   pattern = "Dockerfile.*",
+--   callback = function()
+--     set ft="dockerfile"
+--   end,
+-- })
 
 -- Open nvim-tree on startup
 vim.api.nvim_create_autocmd({ "VimEnter" }, {
