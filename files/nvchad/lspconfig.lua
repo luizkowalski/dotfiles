391788dc35ff397c62bd796e43ea69af99f8d2e6
Luiz Eduardo Kowalski <luizeduardokowalski@gmail.com>
2023-12-07 00:10:07 +0100

    Adding yamlls, fixing ruby_ls

--- files/nvchad/lspconfig.lua
+++ files/nvchad/lspconfig.lua
@@ -7,6 +7,27 @@ local mason_lspconfig = require("mason-lspconfig")
 mason_lspconfig.setup()
 
 lspconfig.dockerls.setup {}
+lspconfig.yamlls.setup {
+  on_attach = on_attach,
+  capabilities = {
+    textDocument = {
+      foldingRange = {
+        dynamicRegistration = false,
+        lineFoldingOnly = true,
+      },
+    },
+  },
+  settings = {
+    redhat = { telemetry = { enabled = false } },
+    yaml = {
+      keyOrdering = false,
+      format = {
+        enable = true,
+      },
+      validate = true
+    }
+  }
+}
 lspconfig.lua_ls.setup {}
 lspconfig.bashls.setup {}
 lspconfig.eslint.setup({
@@ -19,20 +40,54 @@ lspconfig.eslint.setup({
   end,
 })
 
+_timers = {}
+local function setup_diagnostics(client, buffer)
+  if require("vim.lsp.diagnostic")._enable then
+    return
+  end
+
+  local diagnostic_handler = function()
+    local params = vim.lsp.util.make_text_document_params(buffer)
+    client.request("textDocument/diagnostic", { textDocument = params }, function(err, result)
+      if err then
+        local err_msg = string.format("diagnostics error - %s", vim.inspect(err))
+        vim.lsp.log.error(err_msg)
+      end
+      local diagnostic_items = {}
+      if result then
+        diagnostic_items = result.items
+      end
+      vim.lsp.diagnostic.on_publish_diagnostics(
+        nil,
+        vim.tbl_extend("keep", params, { diagnostics = diagnostic_items }),
+        { client_id = client.id }
+      )
+    end)
+  end
+
+  diagnostic_handler() -- to request diagnostics on buffer when first attaching
+
+  vim.api.nvim_buf_attach(buffer, false, {
+    on_lines = function()
+      if _timers[buffer] then
+        vim.fn.timer_stop(_timers[buffer])
+      end
+      _timers[buffer] = vim.fn.timer_start(200, diagnostic_handler)
+    end,
+    on_detach = function()
+      if _timers[buffer] then
+        vim.fn.timer_stop(_timers[buffer])
+      end
+    end,
+  })
+end
+
 lspconfig.ruby_ls.setup({
-  cmd = { "ruby-lsp" },
-  -- enabledFeatures = { "codeActions", "diagnostics", "documentHighlights", "documentSymbols", "formatting", "inlayHint" },
-  -- on_attach = on_attach,
-  capabilities = capabilities,
-  init_options = {
-    formatter = "auto",
-  },
+  on_attach = function(client, buffer)
+    setup_diagnostics(client, buffer)
+  end,
 })
-
--- lspconfig.sorbet.setup({
---   cmd = { "bundle", "exec", "srb", "tc", "--lsp" },
 -- })
-
 lspconfig.tailwindcss.setup({
   cmd = { "./node_modules/.bin/tailwindcss-language-server", "--stdio" },
   on_attach = on_attach,
@@ -45,10 +100,10 @@ lspconfig.tailwindcss.setup({
     -- "javascriptreact",
     -- "typescript",
     -- "typescriptreact",
-    "vue",
+    -- "vue",
     "erb",
     "eruby",
     "postcss",
-    "svelte",
+    -- "svelte",
   },
 })
