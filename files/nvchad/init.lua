c6eaf7f45f5af97d2d3a6aa1ace076bddab862f0
Luiz Eduardo Kowalski <luizeduardokowalski@gmail.com>
2024-02-15 21:23:38 +0100

    yanking

--- files/nvchad/init.lua
+++ files/nvchad/init.lua
@@ -7,3 +7,12 @@ autocmd("VimEnter", {
     -- vim.cmd("colorscheme nvchad")
   end,
 })
+
+-- copy whole file with command called :Y
+autocmd("BufEnter", {
+  pattern = "*",
+  callback = function()
+    vim.cmd("command! -nargs=0 Y :%y+")
+  end,
+})
+--
